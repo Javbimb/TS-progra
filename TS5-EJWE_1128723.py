e1de9f3d2cba321a92e91dd28d2065218efb26f1
Javbimb <143531224+Javbimb@users.noreply.github.com>
2023-09-07 07:56:35 -0600

    Add files via upload

---
+++ TS5-EJWE_1128723.py
@@ -0,0 +1,17 @@
+print ("mi segundo programa")
+print ("Porfavor ingrese su nombre")
+sNombre= input()
+print("ingrese su edad")
+sEdad= input()
+print=("Ingrese su carrera")
+sCarrera=input()
+print("ingrese su carné")
+sCarné=input()
+print =("Nombre"+ sNombre)
+print=("Edad"+sEdad)
+print("Carrera"+sCarrera)
+print("Carne"+sCarné)
+
+print("soy"+sNombre + ",tnego"+ sEdad + "Años y estudio la carrera"+sCarrera + "mi numero de Carné es"+sCarné)
+
+
