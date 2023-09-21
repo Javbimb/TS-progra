6d07ca8733265502d8faaa94fd4ebde89c52cbad
Javbimb <143531224+Javbimb@users.noreply.github.com>
2023-09-21 08:09:43 -0600

    Add files via upload

---
+++ TS7 EJWE 1128723.py
@@ -0,0 +1,35 @@
+print("Erick Javier Wolhers Estrada")
+x=(1)
+
+while x ==1:
+    print("¿Desea un tabla de multiplicar?")
+    print("1: si")
+    print("2: no, salir")
+    op= input()
+    op=int(op)
+
+    if op == 1:
+
+
+     n=input("ingrese un numero de 1 a 10")
+    n=int(n)
+    z=1
+    z=int(z)
+    if n>10: 
+        print("El numero es mayor a 10")
+
+    elif n<1: 
+        print("el numero es menor a 1")
+
+else:
+    for z in range: (1,11)
+
+    print(str(n)+"x"+str(z)+"="+str(n*z))
+   
+
+
+
+
+
+
+
